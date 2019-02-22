79ee1cfbe6857cbe21e4856ef1b316e928a20083
David Pointer <dpointer@directstream.com>
2019-02-22 10:19:08 -0700

    fixed delay clarg parameter, added version clarg, added inifinit ping loop support for count=0

--- connStatus.go
+++ connStatus.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"golang.org/x/net/icmp"
 	"golang.org/x/net/ipv4"
+	"math"
 	"net"
 	"os"
 	"runtime"
@@ -18,19 +19,77 @@ type connectionParameters struct {
 	peer          string        // ping target ipv4 dotted decimal address
 	iface         string        // local interface name
 	count         int           // how many times to ping peer
-	delaySec      time.Duration // with this delay (seconds) between them
+	delay         time.Duration // with this delay between them
 	udpAddr       net.UDPAddr   // full ping target address struct
+	verbose       bool
 }
 
 func main() {
 
 	if unprivilegedICMP() {
 		parameters := initParameters()
-		ConnStatus(&parameters)
+		connStatus(&parameters)
 	}
 }
 
-func ConnStatus(parameters *connectionParameters) {
+// Check if this OS supports unprivileged ICMP.
+func unprivilegedICMP() (retcode bool) {
+
+	retcode = true
+	switch runtime.GOOS {
+	case "darwin": // yes
+		fmt.Println("unprivileged ICMP enabled")
+	case "linux": // maybe?
+		fmt.Println("you may need to adjust the net.ipv4.ping_group_range kernel state")
+	default: // nope
+		fmt.Println("not supported on", runtime.GOOS)
+		retcode = false
+	}
+	return
+}
+
+// Initialize parameters with validated command line args.
+func initParameters() (parameters connectionParameters) {
+
+	parameters = connectionParameters{
+		listenNetwork: "udp4",
+		listenAddress: "0.0.0.0",
+		mtu:           1500,
+		iface:         "en0",
+	}
+	flag.StringVar(&parameters.peer, "peer", "75.75.75.75", "ping target ipv4 address")
+	flag.IntVar(&parameters.count, "count", 1, "non-negative ping repeat count (0 equals forever)")
+	flag.DurationVar(&parameters.delay, "delay", time.Second, "delay (units) between pings")
+	flag.BoolVar(&parameters.verbose, "v", false, "enable verbose output")
+	flag.Parse()
+
+	parameters.udpAddr = checkPeer(parameters.peer, parameters.iface)
+	checkCount(parameters.count)
+
+	fmt.Printf("processed and validated parameters: %+v\n", parameters)
+	return
+}
+
+// error check: ipv4 peer address; create net.UDPAddr struct
+func checkPeer(peer string, iface string) (udpAddr net.UDPAddr) {
+
+	if ipaddr := net.ParseIP(peer); ipaddr == nil {
+		panic(fmt.Sprintf("invalid ipv4 address: %v\n", peer))
+	} else {
+		udpAddr = net.UDPAddr{IP: ipaddr, Zone: iface}
+	}
+	return
+}
+
+// error check: count must be non-negative
+func checkCount(count int) {
+
+	if count < 0 {
+		panic(fmt.Sprintf("invalid negative count: %v\n", count))
+	}
+}
+
+func connStatus(parameters *connectionParameters) {
 
 	// create ICMP listener
 	connection, err := icmp.ListenPacket(parameters.listenNetwork, parameters.listenAddress)
@@ -47,18 +106,28 @@ func ConnStatus(parameters *connectionParameters) {
 	msgRx := make([]byte, parameters.mtu) // receiver data buffer
 	first := true                         // delay only first time through, allow for overflow in loop counter
 
-	// TODO: handle forever pinging
-	// ping/wait for response loop
-	for i := 0; i < parameters.count; i++ {
+	// inc and limits are used to handle forever pinging (count == 0)
+	inc := 1
+	limit := parameters.count
+	if parameters.count == 0 {
+		inc = 0
+		limit = math.MaxInt32
+	}
+	sequence := 0 // icmp ping echo sequence number
 
+	// ping request/wait for response loop
+	for i := 0; i < limit; i += inc {
+
+		// only delay on first pass through loop
 		if first {
 			first = false
 		} else {
-			time.Sleep(parameters.delaySec)
+			time.Sleep(parameters.delay)
 		}
 
 		// create and output next sequenced ICMP echo message to transmit
-		msgTx := createTxMsg(i)
+		msgTx := createTxMsg(sequence)
+		sequence++
 		if _, err := connection.WriteTo(msgTx, &parameters.udpAddr); err != nil {
 			printErr("WriteTo", err)
 			continue
@@ -84,16 +153,13 @@ func ConnStatus(parameters *connectionParameters) {
 				continue
 			}
 
-			// TODO: check seq?
 			// got valid response, interpret the received message type field
+			now := time.Now().Format(time.RFC3339)
 			switch rm.Type {
 			case ipv4.ICMPTypeEchoReply:
 				body, ok := rm.Body.(*icmp.Echo)
-				if ok {
-					now := time.Now().Format(time.RFC3339)
+				if ok && parameters.verbose {
 					fmt.Printf("%v received %v %v from %v\n", now, rm.Type, body.Seq, peer)
-				} else {
-					fmt.Printf("Unable to cast interface icmp.MessageBody to struct icmp.Body\n")
 				}
 			case
 				ipv4.ICMPTypeDestinationUnreachable,
@@ -108,67 +174,11 @@ func ConnStatus(parameters *connectionParameters) {
 				ipv4.ICMPTypePhoturis,
 				ipv4.ICMPTypeExtendedEchoRequest,
 				ipv4.ICMPTypeExtendedEchoReply:
-				fmt.Printf("received unexpected response %v from %v\n", rm.Type, peer)
+				fmt.Printf("%v received unexpected response %v from %v\n", now, rm.Type, peer)
 			default:
-				fmt.Printf("received unknown response %v\n", rm.Type)
+				fmt.Printf("%v received unknown response %v\n", now, rm.Type)
 			}
 		}
-	}
-}
-
-// Check if this OS supports unprivileged ICMP.
-func unprivilegedICMP() (retcode bool) {
-	retcode = true
-	switch runtime.GOOS {
-	case "darwin": // yes
-		fmt.Println("unprivileged ICMP enabled")
-	case "linux": // maybe?
-		fmt.Println("you may need to adjust the net.ipv4.ping_group_range kernel state")
-	default: // nope
-		fmt.Println("not supported on", runtime.GOOS)
-		retcode = false
-	}
-	return
-}
-
-// Initialize parameters with validated command line args.
-func initParameters() (parameters connectionParameters) {
-
-	parameters = connectionParameters{
-		listenNetwork: "udp4",
-		listenAddress: "0.0.0.0",
-		mtu:           1500,
-		iface:         "en0",
-	}
-	flag.StringVar(&parameters.peer, "peer", "75.75.75.75", "ping target ipv4 address")
-	flag.IntVar(&parameters.count, "count", 1, "ping repeat count (non-negative)")
-	flag.DurationVar(&parameters.delaySec, "delaySec", 1, "delay (seconds) between pings")
-	flag.Parse()
-
-	parameters.udpAddr = checkPeer(parameters.peer, parameters.iface)
-	checkCount(parameters.count)
-	parameters.delaySec = parameters.delaySec * time.Second
-
-	fmt.Printf("processed and validated parameters: %+v\n", parameters)
-	return
-}
-
-// error check: ipv4 peer address; create net.UDPAddr struct
-func checkPeer(peer string, iface string) (udpAddr net.UDPAddr) {
-
-	if ipaddr := net.ParseIP(peer); ipaddr == nil {
-		panic(fmt.Sprintf("invalid ipv4 address: %v\n", peer))
-	} else {
-		udpAddr = net.UDPAddr{IP: ipaddr, Zone: iface}
-	}
-	return
-}
-
-// error check: count must be non-negative
-func checkCount(count int) {
-
-	if count < 0 {
-		panic(fmt.Sprintf("invalid negative count: %v\n", count))
 	}
 }
 
